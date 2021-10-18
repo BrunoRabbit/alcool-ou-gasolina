239f70837eb36e4c64d3d483b4168737514687cb
Bruno Coelho <brunoca2000@hotmail.com>
2021-10-18 13:35:35 -0300

    Alcool ou Gasolina

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:alcool_ou_gasolina/components/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: HomeScreen(),
+    );
+  }
+}
