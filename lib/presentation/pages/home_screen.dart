617bb258be598f4a8e16e32b4dc9a2144838a15a
Bruno Coelho <72535437+BrunoRabbit@users.noreply.github.com>
2022-12-06 09:43:06 -0300

    feat: add widget life cicle and minor changes

--- lib/presentation/pages/home_screen.dart
+++ lib/presentation/pages/home_screen.dart
@@ -1,8 +1,6 @@
 import 'package:alcool_ou_gasolina/components/custom_row_input.dart';
 import 'package:flutter/material.dart';
 
-extension DoubleHelper on double {}
-
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -11,43 +9,17 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final TextEditingController _alcoholController = TextEditingController();
+  late TextEditingController _alcoholController;
+  late TextEditingController _gasolineController;
+  late String errorMessage;
 
-  final TextEditingController _gasolineController = TextEditingController();
-
-  String errorMessage = "";
-
-  void calculate() {
-    double? _alcoholValue = double.tryParse(_alcoholController.text);
-    double? _gasolineValue = double.tryParse(_gasolineController.text);
-
-    if (_alcoholValue == null || _gasolineValue == null) {
-      setState(() {
-        errorMessage =
-            "Valor inválido - Insira valores maiores que 0 e separados por ( . )";
-      });
-    } else {
-      if ((_alcoholValue / _gasolineValue) >= 0.7) {
-        setState(() {
-          errorMessage = "Melhor abastecer com Gasolina";
-        });
-      } else {
-        setState(() {
-          errorMessage = "Melhor abastecer com Alcool";
-        });
-      }
-    }
+  @override
+  void initState() {
+    super.initState();
+    _alcoholController = TextEditingController();
+    _gasolineController = TextEditingController();
+    errorMessage = "";
   }
-
-  /*
-
-    Color.fromRGBO(238, 69, 64, 1) - #EE4540 - laranja pouco escuro
-    Color.fromRGBO(199, 44, 65, 1) - #C72C41 - vermelho
-    Color.fromRGBO(128, 19, 54, 1) - #801336 - vermelho escuro
-    Color.fromRGBO(81, 10, 50, 1) - #510A32 - roxo escuro
-    Color.fromRGBO(45,20,44,1) - #2D142C - preto/marrom escuro
-
-  */
 
   @override
   Widget build(BuildContext context) {
@@ -73,7 +45,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
               const Text(
-                "Saiba qua a melhor opção para o abastecimento do seu carro!",
+                "Conheça qual a melhor opção para o abastecimento do seu carro!",
                 style: TextStyle(
                   fontSize: 18,
                 ),
@@ -93,7 +65,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 controller: _gasolineController,
               ),
               const SizedBox(
-                height: 10,
+                height: 15,
               ),
               Row(
                 children: [
@@ -106,15 +78,14 @@ class _HomeScreenState extends State<HomeScreen> {
                         child: const Text(
                           'Calcular',
                           style: TextStyle(
-                            color: Color.fromRGBO(238, 69, 64, 1),
                             fontSize: 15,
                           ),
                         ),
                         style: ElevatedButton.styleFrom(
                           padding: const EdgeInsets.all(20),
-                          primary: const Color.fromRGBO(128, 19, 54, 1),
+                          primary: Colors.purple,
                         ),
-                        onPressed: () => calculate(),
+                        onPressed: calculateBetter,
                       ),
                     ),
                   ),
@@ -134,5 +105,27 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
     );
+  }
+
+  void calculateBetter() {
+    double? _alcoholValue = double.tryParse(_alcoholController.text);
+    double? _gasolineValue = double.tryParse(_gasolineController.text);
+
+    if (_alcoholValue == null || _gasolineValue == null) {
+      setState(() {
+        errorMessage =
+            "Valor inválido - Insira valores maiores que 0 e separados por ( . )";
+      });
+    } else {
+      if ((_alcoholValue / _gasolineValue) >= 0.7) {
+        setState(() {
+          errorMessage = "Melhor abastecer com Gasolina";
+        });
+      } else {
+        setState(() {
+          errorMessage = "Melhor abastecer com Alcool";
+        });
+      }
+    }
   }
 }
