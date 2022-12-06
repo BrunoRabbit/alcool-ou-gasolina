da56713b144fc3bb74d8b9d15607787f3e22742a
Bruno Coelho <72535437+BrunoRabbit@users.noreply.github.com>
2022-12-06 09:54:45 -0300

    chore: code factor

--- lib/components/custom_row_input.dart
+++ lib/components/custom_row_input.dart
@@ -75,16 +75,25 @@ class _CustomRowInputState extends State<CustomRowInput> {
 }
 
 class CustomMoneyFormatter extends TextInputFormatter {
-  static const separator = '.';
+  static const String separator = '.';
+  static String oldValueText = '';
+  static String newValueText = '';
+  static int currentIndex = 0;
+
   @override
   TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    return customFormat(oldValue, newValue);
+  }
+
+  TextEditingValue customFormat(
       TextEditingValue oldValue, TextEditingValue newValue) {
     if (newValue.text.isEmpty) {
       return newValue.copyWith(text: '');
     }
 
-    String oldValueText = oldValue.text.replaceAll(separator, '');
-    String newValueText = newValue.text.replaceAll(separator, '');
+    oldValueText = oldValue.text.replaceAll(separator, '');
+    newValueText = newValue.text.replaceAll(separator, '');
 
     if (oldValue.text.endsWith(separator) &&
         oldValue.text.length == newValue.text.length + 1) {
@@ -92,11 +101,11 @@ class CustomMoneyFormatter extends TextInputFormatter {
     }
 
     if (oldValueText != newValueText) {
-      int selectionIndex =
-          newValue.text.length - newValue.selection.extentOffset;
+      String newString = '';
+
+      currentIndex = newValue.text.length - newValue.selection.extentOffset;
       final chars = newValueText.split('');
 
-      String newString = '';
       for (int i = chars.length - 1; i >= 0; i--) {
         if ((chars.length - 1 - i) % 2 == 0 && i != chars.length - 1) {
           newString = separator + newString;
@@ -107,12 +116,11 @@ class CustomMoneyFormatter extends TextInputFormatter {
       return TextEditingValue(
         text: newString,
         selection: TextSelection.collapsed(
-          offset: newString.length - selectionIndex,
+          offset: newString.length - currentIndex,
         ),
       );
     }
 
-    // If the new value and old value are the same, just return as-is
     return newValue;
   }
 }
